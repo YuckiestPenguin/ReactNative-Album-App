7a8fea23eb84856e733440c02746ee6f645dc1a8
vinicomelo <vinicomelo@gmail.com>
2018-12-10 17:45:48 -0500

    Added album image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,8 +4,8 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({album}) => {
-    const {title, artist, thumbnail_image} = album;
-    const {thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle} = styles;
+    const {title, artist, thumbnail_image, image} = album;
+    const {thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle} = styles;
     return (
         <Card>
             <CardSection>
@@ -16,6 +16,9 @@ const AlbumDetail = ({album}) => {
                     <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+            <CardSection>
+                    <Image style={imageStyle} source={{uri: image}}/>
             </CardSection>
         </Card>
     );
@@ -38,6 +41,11 @@ const styles = {
         alignItems: 'center',
         marginLeft: 10,
         marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
